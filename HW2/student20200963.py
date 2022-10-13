8264e182f6cbecffda5979582d27b359985057b9
NamYeonW00 <cocos121@naver.com>
2022-10-14 06:01:25 +0900

    add hw2 code

--- HW2/student20200963.py
+++ HW2/student20200963.py
@@ -15,37 +15,65 @@ for row in ws:
 		total.append(sum_v) 
 	row_id += 1
 
-total.reverse()
+total.sort(reverse=True)
 
+grade = []
 all = len(total)
 num = all * 0.3
-C = 'A'
+g = 'A0'
 i = 0
-while i < len(total):
+while i < all:
 	if int(num - total.count(total[i])) >= 0:
 		row_id = 2
 		count = 0
 		for row in ws:
 			if total[i] == ws.cell(row_id, column = 7).value:
-				ws.cell(row_id, column = 8).value = C
+				ws.cell(row_id, column = 8).value = g
+				grade.append(g)
 				num -= 1
 				count += 1
 			row_id += 1
 		i += count
-	elif C == 'A':
+	elif g == 'A0':
 		num += all * 0.4
-		C = 'B'
+		g = 'B0'
 	else:
 		break
 
-while i < len(total):
+while i < all:
 	row_id = 2
 	count = 0
 	for row in ws:
 		if total[i] == ws.cell(row_id, column = 7).value:
 			ws.cell(row_id, column = 8).value = 'C'
+			grade.append('C0')
 			count += 1
 		row_id += 1
 	i += count
+
+num = grade.count('A0') * 0.5
+g = 'A+'
+i = 0
+while i < all:
+	if int(num - total.count(total[i])) >= 0:
+		row_id = 2
+		count = 0
+		for row in ws:
+			if total[i] == ws.cell(row_id, column = 7).value:
+				ws.cell(row_id, column = 8).value = g
+				num -= 1
+				count += 1
+			row_id += 1
+		i += count
+	elif g == 'A+':
+		i += grade.count('A0')
+		num = grade.count('B0') * 0.5
+		g = 'B+'
+	elif g == 'B+':
+		i += grade.count('B0')
+		num = grade.count('C0') * 0.5
+		g = 'C+'
+	else:
+		break
 
 wb.save("student.xlsx")
