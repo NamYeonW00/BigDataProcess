94ab2e9806e32efa750ca372a9cdd61c3093e0e7
NamYeonW00 <cocos121@naver.com>
2022-11-02 00:25:15 +0900

    add HW3 code

---
+++ HW3/UBERStudent20200963.py
@@ -0,0 +1,31 @@
+#!usr/bin/python3
+
+import sys
+import calendar
+
+d = dict()
+dayofweek = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+
+file = sys.argv[1]
+
+with open(file, "rt") as fp:
+	for line in fp:
+		str_arr = line.split(",")
+		date = str_arr[1].split("/")
+		day = calendar.weekday(int(date[2]), int(date[0]), int(date[1]))
+
+		rd = str_arr[0] + "," + dayofweek[day]
+		if rd not in d:
+			d[rd] = str_arr[2] + "," + str_arr[3]
+		else:
+			temp = d[rd].split(",")
+			v = int(temp[0]) + int(str_arr[2])
+			t = int(temp[1]) + int(str_arr[3])
+
+fp.close()
+
+file = sys.argv[2]
+with open(file, "wt") as fp:
+	for k, v in d.items():
+		data = "%s %s" % (k, v)
+		fp.write(data)
